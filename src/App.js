e9d89a9eb051c7fd125a9942e76a4e9565e8a1ae
nescatfe <fajarsnick@gmail.com>
2024-06-28 11:20:59 +0700

    links

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <p>Based on famous <a href="https://waitbutwhy.com/2014/05/life-weeks.html" target="_blank" rel="noopener noreferrer">Your Life in Weeks</a> Article by <a href="https://waitbutwhy.com" target="_blank" rel="noopener noreferrer">Wait but Why</a>
 </p>
         <p>
-          This app still under development, check out the code on <a href="https://ubur.my.id" target="_blank" rel="noopener noreferrer">Github</a> by <a href="https://ubur.my.id" target="_blank" rel="noopener noreferrer">Malvi</a>
+          This app still under development, check out the code on <a href="https://github.com/nescatfe/Your-Life-in-Weeks" target="_blank" rel="noopener noreferrer">Github</a> by <a href="https://ubur.my.id" target="_blank" rel="noopener noreferrer">Malvi</a>
         </p>
       </footer>
     </div>
